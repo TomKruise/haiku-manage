318f37418d962f76a04c90308a089b664bf5e51e
Tom <kouzhenju@163.com>
2024-09-30 21:20:06 +0900

    First commit!

---
+++ haoke-manage-dubbo-server/haoke-manage-dubbo-server-house-resources/resources-dubbo-interface/src/main/java/com/tom/haoke/dubbo/server/pojo/BasePojo.java
@@ -0,0 +1,12 @@
+package com.tom.haoke.dubbo.server.pojo;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public abstract class BasePojo implements java.io.Serializable {
+
+    private Date created;
+    private Date updated;
+
+}
